48b0c3be533b8762097c2185a7161434952d9c48
Rafał Kamiński <r.kaminski008@gmail.com>
2022-10-04 12:46:39 +0000

    Upgrade

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,19 +1,11 @@
 if status is-interactive
-    # Commands to run in interactive sessions can go here
+    starship init fish | source
 end
-
-fish_add_path /opt/homebrew/bin
-fish_add_path /opt/homebrew/opt/coreutils/libexec/gnubin 
-fish_add_path /Users/rafalkaminski/Library/Python/3.8/bin
-
-source /opt/homebrew/opt/asdf/libexec/asdf.fish
 
 thefuck --alias | source
 
 alias unfuck='stty sane'
 
-starship init fish | source
-
-set -xU JAVA_HOME /Library/Java/JavaVirtualMachines/jdk-18.0.2.jdk/Contents/Home
-
-set -xU FZF_DEFAULT_OPTS '--height 40% --layout=reverse --border -m'
+set -U FZF_DEFAULT_OPTS '--height 40% --layout=reverse --border -m'
+set -U FZF_COMPLETE 2
+set -U FZF_FIND_FILE_COMMAND "ag -l --hidden --ignore .git"
